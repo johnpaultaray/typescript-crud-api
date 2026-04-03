51b19c6df13fae5a3bb1141d12780e42f76365e7
John Paul Taray <your_email@example.com>
2026-04-03 21:04:07 +0800

    Lacking Testing Setup

---
+++ src/_middleware/validateRequest.ts
@@ -0,0 +1,28 @@
+//src/_middleware/validateRequest.ts
+import type { Request, NextFunction } from "express";
+import Joi from "joi";
+
+export function validateRequest(
+  req: Request,
+  next: NextFunction,
+  schema: Joi.ObjectSchema
+): void {
+  const options = {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true,
+  };
+
+  const { error, value } = schema.validate(req.body, options);
+
+  if (error) {
+    next(
+      `Validation error: ${error.details
+        .map((d) => `${d.path.join(".")} is required`)
+        .join(", ")}`
+    );
+  } else {
+    req.body = value;
+    next();
+  }
+}
